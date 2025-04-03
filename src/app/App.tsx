315becfd37b01a65c548b716b1d8211005146a3c
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-04-03 10:44:41 -0300

    start project

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+
+      <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-green-500">
+    <h1 className="text-5xl text-red-500 uppercase font-bold">project The Wind Oasis</h1>
+      </div>
+   
+  )
+}
+
+export default App
