7dd2b4001598de295e628fe4015b23c9373e3bba
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-04-23 19:38:52 -0300

    🔥chore: update dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,7 +7,7 @@ function Dashboard() {
   return (
     <>
       <Row type="horizontal">
-        <Heading as="h1">Dashboard</Heading>
+        <Heading as="h1">Project Status Dashboard</Heading>
         <DashboardFilter />
       </Row>
 
